9783c1df595b0d9b607160e3e598178539d2622d
daikimatsuoka <daiki19981115@gmail.com>
2023-08-07 08:44:49 +0900

    ストロングパラメーターの設定・バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :encrypted_password, with: PASSWORD_REGEX
+  validates_format_of :password, with: PASSWORD_REGEX
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
     validates :surname
@@ -13,8 +13,8 @@ class User < ApplicationRecord
   end
 
   with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/} do
-    validates :surname_kana, presence: true
-    validates :first_name_kana, presence: true
+    validates :surname_kana
+    validates :first_name_kana
   end
   
   validates :date_of_birth, presence: true
