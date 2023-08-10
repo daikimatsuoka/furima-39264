82fe379e2e10ee5794a58b40c1bce7f799f9fffc
daikimatsuoka <daiki19981115@gmail.com>
2023-08-10 09:51:28 +0900

    バリデーションの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,12 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :description_item, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :shipping_charges_id, presence: true
+  validates :region_id, presence: true
+  validates :price, presence: true
+  validates :item_name, presence: true
+
   belongs_to :user
 end
