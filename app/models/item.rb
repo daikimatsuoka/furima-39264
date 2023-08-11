353ade05888108f8daa4d02256fbca5288c9c5e0
daikimatsuoka <daiki19981115@gmail.com>
2023-08-12 08:16:44 +0900

    Activehashのバリデーション、アソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   validates :item_name, presence: true
   validates :description_item, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :shipping_charges_id, presence: true
-  validates :region_id, presence: true
-  validates :number_of_day_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :shipping_charges_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :region_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :number_of_day_id, presence: true ,numericality: { other_than: 1 , message: "can't be blank" }
   validates :price, presence: true
   validates :item_name, presence: true
 
--- app/models/number_of_day.rb
+++ app/models/number_of_day.rb
@@ -5,4 +5,8 @@ class NumberOfDay< ActiveHash::Base
     { id: 3, name: '2~3"日で発送' },
     { id: 4, name: '4~7"日で発送' }
   ]
+
+  include ActiveHash::Associations
+  has_many :items
+  
   end
