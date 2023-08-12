f7234956eb57f5ac127987c1984dc64401ddb924
daikimatsuoka <daiki19981115@gmail.com>
2023-08-12 10:08:45 +0900

    バリデーションの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,9 +6,10 @@ class Item < ApplicationRecord
   validates :shipping_charges_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :region_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :number_of_day_id, presence: true ,numericality: { other_than: 1 , message: "can't be blank" }
-  validates :price, presence: true
+  validates :price, presence: true,format: {with: /\A[0-9]+\z/ },numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
   validates :item_name, presence: true
 
   belongs_to :user
-  has_one_attached :image
+  has_one_attached :item_image
+
 end
