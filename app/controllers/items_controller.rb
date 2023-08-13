c906d4a4847b958de0186389d03527fa495339fb
daikimatsuoka <daiki19981115@gmail.com>
2023-08-13 10:22:12 +0900

    ビューに関する記述とそれに伴うエラーメッセージの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
     
   end
@@ -8,6 +10,17 @@ class ItemsController < ApplicationController
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :description_item, :category_id, :condition_id, :shipping_charges_id, :region_id, :number_of_day_id, :price, :item_image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,17 @@ class Item < ApplicationRecord
   validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :shipping_charges_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :region_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
-  validates :number_of_day_id, presence: true ,numericality: { other_than: 1 , message: "can't be blank" }
+  validates :number_of_day_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
   validates :price, presence: true,format: {with: /\A[0-9]+\z/ },numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
-  validates :item_name, presence: true
+  validates :item_image, presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :condition
+  belongs_to :category
+  belongs_to :shipping_charges
+  belongs_to :region
+  belongs_to :number_of_day
+
 
   belongs_to :user
   has_one_attached :item_image
