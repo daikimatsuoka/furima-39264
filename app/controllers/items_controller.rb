29152b8ad71a5e881bdc2a5fd194ab5526882cb7
daikimatsuoka <daiki19981115@gmail.com>
2023-08-18 09:28:11 +0900

    身が出品していない商品の商品情報編集ページへ遷移機能の実装
     ログイン状態の場合でも、自身が出品していない商品の商品情報編集ページへ遷移しようとすると、商品の販売状況に関わらずトップページに遷移すること。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.order('created_at DESC')
@@ -41,5 +42,12 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :description_item, :category_id, :condition_id, :shipping_charges_id, :region_id,
                                  :number_of_day_id, :price, :item_image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user_id
+      redirect_to root_path
+    end
   end
 end
