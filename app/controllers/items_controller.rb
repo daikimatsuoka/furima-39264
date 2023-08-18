6bdc541d8b7b8a99c45f0cf47a89617e97000414
daikimatsuoka <daiki19981115@gmail.com>
2023-08-18 14:28:58 +0900

    Rubocopの実行

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,9 +44,9 @@ class ItemsController < ApplicationController
 
   def move_to_index
     @item = Item.find(params[:id])
-    unless user_signed_in? && current_user.id == @item.user_id
-      redirect_to root_path
-    end
+    return if user_signed_in? && current_user.id == @item.user_id
+
+    redirect_to root_path
   end
 
   def set_item
