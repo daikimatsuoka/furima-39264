90faacc8a7395df00b0f5933a57d5785e6ef0925
daikimatsuoka <daiki19981115@gmail.com>
2023-09-05 10:27:20 +0900

    エラーメッセージの日本語化

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,11 @@
 class Item < ApplicationRecord
   validates :item_name, presence: true, length: { maximum: 40 }
   validates :description_item, presence: true, length: { maximum: 1000 }
-  validates :category_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :condition_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :shipping_charges_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :region_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
-  validates :number_of_day_id, presence: true, numericality: { other_than: 1, message: "can't be blank" }
+  validates :category_id, presence: true, numericality: { other_than: 1, message: "を入力してください" }
+  validates :condition_id, presence: true, numericality: { other_than: 1, message: "を入力してください" }
+  validates :shipping_charges_id, presence: true, numericality: { other_than: 1, message: "を入力してください" }
+  validates :region_id, presence: true, numericality: { other_than: 1, message: "を入力してください" }
+  validates :number_of_day_id, presence: true, numericality: { other_than: 1, message: "を入力してください" }
   validates :price, presence: true,
                     numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   validates :item_image, presence: true
